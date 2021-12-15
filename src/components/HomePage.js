35ef282044a38711f03a1d9a8bc9fc893173bc27
DellaDominic <delladominic33@gmail.com>
2021-12-15 20:11:45 +0530

    add HomePage component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage(){
+    return(
+        <div className='jumbotron'>
+            <h1>Pluralsight Administration</h1>
+            <p>React,flux and React router for responsive web applications</p>
+        </div>
+    );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import {render} from 'react-dom';
+import HomePage from "./components/HomePage";
+
+
+render(
+    <HomePage />,
+    document.getElementById("root")
+);
