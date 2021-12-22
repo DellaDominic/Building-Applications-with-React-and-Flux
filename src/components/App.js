a0a0fd4b6951e55316f3c0d96c9b33111f78d5cd
DellaDominic <delladominic33@gmail.com>
2021-12-22 14:21:44 +0530

    add Header.js for navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 const route = window.location.pathname;
 
 function App() {
-  if (route === "/about") return <AboutPage />;
-  return <HomePage />;
+  function getPage() {
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
