0bd312c450db64f669cedeafa00ed6a834bf49c0
DellaDominic <delladominic33@gmail.com>
2021-12-27 17:05:03 +0530

    added coursePage

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,8 +1,43 @@
 import React from "react";
+import { getCourses } from "../api/courseApi";
 
 class CoursesPage extends React.Component {
+  state = {
+    courses: [],
+  };
+
+  componentDidMount() {
+    getCourses().then((courses) => {
+      this.setState({ courses: courses });
+    });
+  }
+
   render() {
-    return <h1> Courses </h1>;
+    return (
+      <React.Fragment>
+        <h1> Courses </h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                <tr key={course.id}>
+                  <th>{course.title}</th>
+                  <th>{course.authorId}</th>
+                  <th>{course.category}</th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
   }
 }
 
