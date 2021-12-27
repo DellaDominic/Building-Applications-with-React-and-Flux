8738c615805b9af11f2d659f994cadb88b9d60a9
DellaDominic <delladominic33@gmail.com>
2021-12-27 17:24:29 +0530

    class to function component convertion for CoursesPage

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,44 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: [],
-  };
+function CoursesPage() {
+  const [courses, setCourse] = useState([]);
 
-  componentDidMount() {
-    getCourses().then((courses) => {
-      this.setState({ courses: courses });
+  useEffect(() => {
+    getCourses().then((_courses) => {
+      setCourse(_courses);
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <React.Fragment>
-        <h1> Courses </h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              return (
-                <tr key={course.id}>
-                  <th>{course.title}</th>
-                  <th>{course.authorId}</th>
-                  <th>{course.category}</th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <h1> Courses </h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => {
+            return (
+              <tr key={course.id}>
+                <th>{course.title}</th>
+                <th>{course.authorId}</th>
+                <th>{course.category}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 }
 
 export default CoursesPage;
