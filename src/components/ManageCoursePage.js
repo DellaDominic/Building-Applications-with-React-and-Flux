6c441317a0373ad8469c95f18ee5fc5f6ce3e6f6
DellaDominic <delladominic33@gmail.com>
2022-01-01 23:53:01 +0530

    add course state using useState

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import CourseForm from "./CourseForm";
 
 const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
   return (
     <React.Fragment>
       <h1>Manage Course</h1>
