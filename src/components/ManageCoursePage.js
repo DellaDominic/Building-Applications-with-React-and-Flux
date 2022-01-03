1fb98c7c391d4cc7b0224e4c9e63bc921832209f
DellaDominic <delladominic33@gmail.com>
2022-01-03 09:10:18 +0530

    handleSubmit added

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
 
 const ManageCoursePage = (props) => {
   const [course, setCourse] = useState({
@@ -13,10 +14,19 @@ const ManageCoursePage = (props) => {
   function handleChange({ target }) {
     setCourse({ ...course, [target.name]: target.value });
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course);
+  }
   return (
     <React.Fragment>
       <h1>Manage Course</h1>
-      <CourseForm course={course} onChange={handleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </React.Fragment>
   );
 };
